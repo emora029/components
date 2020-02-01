d40edf2789360c78549d6d1fb7a86a06e34807b1
Elizabeth Mora <emora029@gmail.com>
2020-01-31 22:59:46 -0400

    Card Widget

---
+++ lib/src/pages/card_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards Page'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          _cardTipo1()
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo1() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.photo_album, color: Colors.blue),
+            title: Text('Lorem Ipsum'),
+            subtitle: Text('Is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Cancelar'),
+                onPressed: (){},
+              ),
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: (){},
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+}
